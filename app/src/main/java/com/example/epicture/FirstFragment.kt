f2a7e3d02c6260cec5990035b970caef085a0309
Victor S <victor.simon@epitech.eu>
2020-10-15 18:31:11 +0200

    ADD - basic id and password components

--- app/src/main/java/com/example/epicture/FirstFragment.kt
+++ app/src/main/java/com/example/epicture/FirstFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.navigation.fragment.findNavController
 
 /**
@@ -24,8 +25,22 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        var identifiant_componant = view.findViewById<EditText>(R.id.identifiant);
+        var password_componant = view.findViewById<EditText>(R.id.password);
+        var id = "";
+        var pass = "";
+
+        // change view if button clicked
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            id = identifiant_componant.toString();
+            pass = password_componant.toString();
+        }
+        identifiant_componant.setOnClickListener {
+            identifiant_componant.setText("");
+        }
+        password_componant.setOnClickListener {
+            password_componant.setText("");
         }
     }
 }
